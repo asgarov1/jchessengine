36ad917cc4d69ea93e2106e0531efdc279853ef1
Javid Asgarov <asgarov1@gmail.com>
2026-01-14 00:10:59 +0100

    Update Engine to allow move from String

--- Sources/JChessEngine/Move.swift
+++ Sources/JChessEngine/Move.swift
@@ -15,4 +15,17 @@ public struct Move {
         self.to = to
         self.type = type
     }
+    
+    public init(from: String, to: String, type: MoveType = .normal) {
+        self.from = Move.squareIndex(from)
+        self.to = Move.squareIndex(to)
+        self.type = type
+    }
+
+    private static func squareIndex(_ s: String) -> Int {
+        let chars = Array(s.lowercased())
+        let file = Int(chars[0].unicodeScalars.first!.value - Character("a").unicodeScalars.first!.value)
+        let rank = Int(chars[1].unicodeScalars.first!.value - Character("1").unicodeScalars.first!.value)
+        return rank * 8 + file
+    }
 }
